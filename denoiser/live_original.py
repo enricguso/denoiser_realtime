69488214edb159e8e290506c809f20acf825800f
enricguso <enricguso@gmail.com>
2023-06-07 19:09:08 +0200

    added the offline test script

--- denoiser/live_original.py
+++ denoiser/live_original.py
@@ -10,16 +10,18 @@ import sys
 
 import sounddevice as sd
 import torch
-import time
-import causal_improved_sudormrf_v3 
+
+from .demucs import DemucsStreamer
+from .pretrained import add_model_flags, get_model
+from .utils import bold
 
 
 def get_parser():
     parser = argparse.ArgumentParser(
-        "sudo-rm-rf.live",
+        "denoiser.live",
         description="Performs live speech enhancement, reading audio from "
                     "the default mic (or interface specified by --in) and "
-                    "writing the enhanced version to the output device "
+                    "writing the enhanced version to 'Soundflower (2ch)' "
                     "(or the interface specified by --out)."
         )
     parser.add_argument(
@@ -28,30 +30,24 @@ def get_parser():
     parser.add_argument(
         "-o", "--out", default="Soundflower (2ch)",
         help="name or index of output interface.")
-    #add_model_flags(parser)
+    add_model_flags(parser)
     parser.add_argument(
         "--no_compressor", action="store_false", dest="compressor",
         help="Deactivate compressor on output, might lead to clipping.")
     parser.add_argument(
-        "--device", default="cpu", help='cpu, cuda or mps')
-    #parser.add_argument(
-    #    "--dry", type=float, default=0.04,
-    #    help="Dry/wet knob, between 0 and 1. 0=maximum noise removal "
-    #         "but it might cause distortions. Default is 0.04")
+        "--device", default="cpu")
     parser.add_argument(
-        "-t", "--num_threads", type=int, default=1,
+        "--dry", type=float, default=0.04,
+        help="Dry/wet knob, between 0 and 1. 0=maximum noise removal "
+             "but it might cause distortions. Default is 0.04")
+    parser.add_argument(
+        "-t", "--num_threads", type=int,
         help="Number of threads. If you have DDR3 RAM, setting -t 1 can "
              "improve performance.")
     parser.add_argument(
         "-f", "--num_frames", type=int, default=1,
         help="Number of frames to process at once. Larger values increase "
              "the overall lag, but will improve speed.")
-    parser.add_argument(
-        '-b', '--bypass', action='store_true',
-        help='bypass the model application')
-    parser.add_argument(
-        '-n', '--noise', action='store_true',
-        help='use noise as input instead')
     return parser
 
 
@@ -81,36 +77,21 @@ def query_devices(device, kind):
 
 
 def main():
-    fs = 16000
-    length = 256
     args = get_parser().parse_args()
     if args.num_threads:
         torch.set_num_threads(args.num_threads)
 
-    model = causal_improved_sudormrf_v3.CausalSuDORMRF(
-        in_audio_channels=1,
-        out_channels=512,
-        in_channels=256,
-        num_blocks=16,
-        upsampling_depth=5,
-        enc_kernel_size=21,
-        enc_num_basis=512,
-        num_sources=1,
-        )
-    
-    model = torch.nn.DataParallel(model)
-    model.load_state_dict(torch.load('e39_sudo_whamr_16k_enhnoisy_augment.pt', map_location=args.device))
-    model = model.module.to(args.device)
+    model = get_model(args).to(args.device)
     model.eval()
-
     print("Model loaded.")
+    streamer = DemucsStreamer(model, dry=args.dry, num_frames=args.num_frames)
 
     device_in = parse_audio_device(args.in_)
     caps = query_devices(device_in, "input")
     channels_in = min(caps['max_input_channels'], 2)
     stream_in = sd.InputStream(
         device=device_in,
-        samplerate=fs,
+        samplerate=model.sample_rate,
         channels=channels_in)
 
     device_out = parse_audio_device(args.out)
@@ -118,58 +99,54 @@ def main():
     channels_out = min(caps['max_output_channels'], 2)
     stream_out = sd.OutputStream(
         device=device_out,
-        samplerate=fs,
+        samplerate=model.sample_rate,
         channels=channels_out)
 
     stream_in.start()
     stream_out.start()
-    #first = True
-    #current_time = 0
-    #last_log_time = 0
-    #last_error_time = 0
-    #cooldown_time = 2
-    #log_delta = 10
-    #sr_ms = fs / 1000
-    #stride_ms = stride / sr_ms
-    #print(f"Ready to process audio, total lag: {streamer.total_length / sr_ms:.1f}ms.")
-
+    first = True
+    current_time = 0
+    last_log_time = 0
+    last_error_time = 0
+    cooldown_time = 2
+    log_delta = 10
+    sr_ms = model.sample_rate / 1000
+    stride_ms = streamer.stride / sr_ms
+    print(f"Ready to process audio, total lag: {streamer.total_length / sr_ms:.1f}ms.")
     while True:
         try:
+            if current_time > last_log_time + log_delta:
+                last_log_time = current_time
+                tpf = streamer.time_per_frame * 1000
+                rtf = tpf / stride_ms
+                print(f"time per frame: {tpf:.1f}ms, ", end='')
+                print(f"RTF: {rtf:.1f}")
+                streamer.reset_time_per_frame()
+
+            length = streamer.total_length if first else streamer.stride
+            first = False
+            current_time += length / model.sample_rate
             frame, overflow = stream_in.read(length)
             frame = torch.from_numpy(frame).mean(dim=1).to(args.device)
-            if args.noise: 
-                frame = torch.randn_like(frame)
-            if not args.bypass:
-                tic = time.time()
-                ini_nrg = torch.sum(frame ** 2)
-                frame = (frame - torch.mean(frame)) / torch.std(frame) #OPTIONAL NORMALIZATION 
-                with torch.no_grad():
-                    out = model(frame.unsqueeze(0).unsqueeze(0)).detach().squeeze()
-                out /= torch.sqrt(torch.sum(out ** 2) / ini_nrg) #energy constraint
-                tac = time.time()
-                #print((length/fs)/1000-((tac - tic)/1000)) #spare time!
-            else:
-                out = frame
-            out = out.unsqueeze(1).repeat(1, 2) # upmix to stereo
-
-
+            with torch.no_grad():
+                out = streamer.feed(frame[None])[0]
             if not out.numel():
                 continue
             if args.compressor:
                 out = 0.99 * torch.tanh(out)
-            #out = out[:, None].repeat(1, channels_out)
+            out = out[:, None].repeat(1, channels_out)
             mx = out.abs().max().item()
             if mx > 1:
                 print("Clipping!!")
             out.clamp_(-1, 1)
             out = out.cpu().numpy()
             underflow = stream_out.write(out)
-            #if overflow or underflow:
-            #    if current_time >= last_error_time + cooldown_time:
-            #        last_error_time = current_time
-            #        tpf = 1000 * streamer.time_per_frame
-            #        print(f"Not processing audio fast enough, time per frame is {tpf:.1f}ms "
-            #              f"(should be less than {stride_ms:.1f}ms).")
+            if overflow or underflow:
+                if current_time >= last_error_time + cooldown_time:
+                    last_error_time = current_time
+                    tpf = 1000 * streamer.time_per_frame
+                    print(f"Not processing audio fast enough, time per frame is {tpf:.1f}ms "
+                          f"(should be less than {stride_ms:.1f}ms).")
         except KeyboardInterrupt:
             print("Stopping")
             break
